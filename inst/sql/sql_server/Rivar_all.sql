5d1a6d9a187feb6f59771caeb62a41b99e9546a7
Stefan Payralbe <stefan@thehyve.nl>
2016-09-21 16:52:39 +0200

    Updated study start date of cohort SQL scripts

--- inst/sql/sql_server/Rivar_all.sql
+++ inst/sql/sql_server/Rivar_all.sql
@@ -1,19 +1,7 @@
-/*********************************************************************************
-# Copyright 2014-2015 Observational Health Data Sciences and Informatics
-#
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-********************************************************************************/
+-- Cohort for the rivaroxaban treatment group exported from ATLAS.
+-- Drug exposure of Rivaroxaban on or after October 3, 2012
+-- Age Greater or equal to 18
+-- Condition occurrence of atrial fibrillation before October 3, 2012
 
 CREATE TEMP TABLE Codesets  (
   codeset_id int NOT NULL,
@@ -24,12 +12,29 @@ CREATE TEMP TABLE Codesets  (
 INSERT INTO Codesets (codeset_id, concept_id)
 SELECT 0 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
 ( 
-  select concept_id from @cdm_database_schema.CONCEPT where concept_id in (45777059,45777060,40241331,40241332,40241333,40241334,40241335,40244443,43535203,40244444,40244445,40244446,40244447,43535205,40244448,40244449,40244450,44657356,44580574,44678535,44560021,40241336,40241337)and invalid_reason is null
+  select concept_id from @cdm_database_schema.CONCEPT where concept_id in (40241331)and invalid_reason is null
+UNION  select c.concept_id
+  from @cdm_database_schema.CONCEPT c
+  join @cdm_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
+  and ca.ancestor_concept_id in (40241331)
+  and c.invalid_reason is null
+
+) I
+) C;
+INSERT INTO Codesets (codeset_id, concept_id)
+SELECT 1 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
+( 
+  select concept_id from @cdm_database_schema.CONCEPT where concept_id in (313217)and invalid_reason is null
+UNION  select c.concept_id
+  from @cdm_database_schema.CONCEPT c
+  join @cdm_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
+  and ca.ancestor_concept_id in (313217)
+  and c.invalid_reason is null
 
 ) I
 ) C;
 
-CREATE TEMP TABLE PrimaryCriteriaEvents
+CREATE TEMP TABLE primary_events
 
 AS
 SELECT
@@ -48,8 +53,9 @@ from
   FROM @cdm_database_schema.DRUG_EXPOSURE de
 where de.drug_concept_id in (SELECT concept_id from  Codesets where codeset_id = 0)
 ) C
-
-
+JOIN @cdm_database_schema.PERSON P on C.person_id = P.person_id
+WHERE C.drug_exposure_start_date >= TO_DATE(TO_CHAR(2012,'0000')||'-'||TO_CHAR( 10,'00')||'-'||TO_CHAR( 03,'00'), 'YYYY-MM-DD')
+AND EXTRACT(YEAR FROM C.drug_exposure_start_date) - P.year_of_birth >= 18
 
   ) P
 ) P
@@ -58,7 +64,7 @@ WHERE (OP.OBSERVATION_PERIOD_START_DATE + 0) <= P.START_DATE AND (P.START_DATE +
 ;
 
 
-CREATE TEMP TABLE cohort_candidate
+CREATE TEMP TABLE qualified_events
 
 AS
 SELECT
@@ -67,16 +73,44 @@ SELECT
 FROM
  
 (
-  select RawEvents.*, row_number() over (partition by RawEvents.person_id order by RawEvents.start_date ASC) as ordinal
-  FROM
+  select pe.event_id, pe.person_id, pe.start_date, pe.end_date, pe.op_start_date, pe.op_end_date, row_number() over (partition by pe.person_id order by pe.start_date ASC) as ordinal
+  FROM primary_events pe
+  
+JOIN (
+select 0 as index_id, event_id
+FROM
+(
+  select event_id FROM
   (
-    select pe.event_id, pe.person_id, pe.start_date, pe.end_date, pe.op_start_date, pe.op_end_date
-    FROM PrimaryCriteriaEvents pe
-    
-  ) RawEvents
-) Results
+    SELECT 0 as index_id, p.event_id
+FROM primary_events P
+LEFT JOIN
+(
+  select C.person_id, C.condition_start_date as start_date, COALESCE(C.condition_end_date, (C.condition_start_date + 1)) as end_date, C.CONDITION_CONCEPT_ID as TARGET_CONCEPT_ID
+from 
+(
+        select co.*, ROW_NUMBER() over (PARTITION BY co.person_id ORDER BY co.condition_start_date) as ordinal
+        FROM @cdm_database_schema.CONDITION_OCCURRENCE co
+where co.condition_concept_id in (SELECT concept_id from  Codesets where codeset_id = 1)
+) C
+
+WHERE C.condition_start_date < TO_DATE(TO_CHAR(2012,'0000')||'-'||TO_CHAR( 10,'00')||'-'||TO_CHAR( 03,'00'), 'YYYY-MM-DD')
+
+) A on A.person_id = P.person_id and A.START_DATE BETWEEN P.OP_START_DATE AND P.OP_END_DATE AND A.START_DATE BETWEEN P.OP_START_DATE and P.OP_END_DATE
+GROUP BY p.event_id
+HAVING COUNT(A.TARGET_CONCEPT_ID) >= 1
+
+
+  ) CQ
+  GROUP BY event_id
+  HAVING COUNT(index_id) = 1
+) G
+) AC on AC.event_id = pe.event_id
+
+) QE
 
 ;
+
 
 CREATE TEMP TABLE inclusionRuleCohorts 
  (
@@ -86,70 +120,70 @@ CREATE TEMP TABLE inclusionRuleCohorts
 ;
 
 
--- the matching group with all bits set ( POWER(2,# of inclusion rules) - 1 = inclusion_rule_mask
+CREATE TEMP TABLE included_events
+
+AS
+WITH  cteIncludedEvents(event_id, person_id, start_date, end_date, op_start_date, op_end_date, ordinal)  AS 
+(
+  SELECT event_id, person_id, start_date, end_date, op_start_date, op_end_date, row_number() over (partition by person_id order by start_date ASC) as ordinal
+  from
+  (
+    select Q.event_id, Q.person_id, Q.start_date, Q.end_date, Q.op_start_date, Q.op_end_date, SUM(coalesce(POWER(cast(2 as bigint), I.inclusion_rule_id), 0)) as inclusion_rule_mask
+    from qualified_events Q
+    LEFT JOIN inclusionRuleCohorts I on I.event_id = Q.event_id
+    GROUP BY Q.event_id, Q.person_id, Q.start_date, Q.end_date, Q.op_start_date, Q.op_end_date
+  ) MG -- matching groups
+
+)
+ SELECT
+ event_id, person_id, start_date, end_date, op_start_date, op_end_date
+
+FROM
+ cteIncludedEvents Results
+
+;
+
+-- Apply end date stratagies
+-- by default, all events extend to the op_end_date.
+CREATE TEMP TABLE cohort_ends
+
+AS
+SELECT
+ event_id, person_id, op_end_date as end_date
+
+FROM
+ included_events;
+
+
+
 DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition_id = @target_cohort_id;
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, cohort_start_date, cohort_end_date)
-select @target_cohort_id as cohort_definition_id, MG.person_id, MG.start_date, MG.end_date
-from
-(
-  select C.event_id, C.person_id, C.start_date, C.end_date, SUM(coalesce(POWER(cast(2 as bigint), I.inclusion_rule_id), 0)) as inclusion_rule_mask
-  from cohort_candidate C
-  LEFT JOIN inclusionRuleCohorts I on I.event_id = C.event_id
-  GROUP BY C.event_id, C.person_id, C.start_date, C.end_date
-) MG -- matching groups
-
+select @target_cohort_id as cohort_definition_id, F.person_id, F.start_date, F.end_date
+FROM (
+  select Q.person_id, Q.start_date, E.end_date, row_number() over (partition by E.person_id order by E.end_date) as ordinal 
+  from qualified_events Q
+  join cohort_ends E on Q.event_id = E.event_id and Q.person_id = E.person_id and E.end_date >= Q.start_date
+) F
+WHERE F.ordinal = 1
 ;
 
--- calculte matching group counts
-delete from @results_database_schema.cohort_inclusion_result where cohort_definition_id = @target_cohort_id;
-insert into @results_database_schema.cohort_inclusion_result (cohort_definition_id, inclusion_rule_mask, person_count)
-select @target_cohort_id as cohort_definition_id, inclusion_rule_mask, count(*) as person_count
-from
-(
-  select C.event_id, SUM(coalesce(POWER(cast(2 as bigint), I.inclusion_rule_id), 0)) as inclusion_rule_mask
-  from cohort_candidate C
-  LEFT JOIN inclusionRuleCohorts I on c.event_id = i.event_id
-  GROUP BY C.event_id
-) MG -- matching groups
-group by inclusion_rule_mask
-;
 
--- calculate gain counts
-delete from @results_database_schema.cohort_inclusion_stats where cohort_definition_id = @target_cohort_id;
-insert into @results_database_schema.cohort_inclusion_stats (cohort_definition_id, rule_sequence, person_count, gain_count, person_total)
-select ir.cohort_definition_id, ir.rule_sequence, coalesce(T.person_count, 0) as person_count, coalesce(SR.person_count, 0) gain_count, EventTotal.total
-from @results_database_schema.cohort_inclusion ir
-left join
-(
-  select i.inclusion_rule_id, count(i.event_id) as person_count
-  from cohort_candidate C
-  JOIN inclusionRuleCohorts i on C.event_id = i.event_id
-  group by i.inclusion_rule_id
-) T on ir.rule_sequence = T.inclusion_rule_id
-CROSS JOIN (select count(*) as total_rules from @results_database_schema.cohort_inclusion where cohort_definition_id = @target_cohort_id) RuleTotal
-CROSS JOIN (select count(event_id) as total from cohort_candidate) EventTotal
-LEFT JOIN @results_database_schema.cohort_inclusion_result SR on SR.cohort_definition_id = @target_cohort_id AND (POWER(cast(2 as bigint),RuleTotal.total_rules) - POWER(cast(2 as bigint),ir.rule_sequence) - 1) = SR.inclusion_rule_mask -- POWER(2,rule count) - POWER(2,rule sequence) - 1 is the mask for 'all except this rule' 
-WHERE ir.cohort_definition_id = @target_cohort_id
-;
 
--- calculate totals
-delete from @results_database_schema.cohort_summary_stats where cohort_definition_id = @target_cohort_id;
-insert into @results_database_schema.cohort_summary_stats (cohort_definition_id, base_count, final_count)
-select @target_cohort_id as cohort_definition_id, 
-(select count(event_id) as total from cohort_candidate) as person_count,
-coalesce((
-  select sr.person_count 
-  from @results_database_schema.cohort_inclusion_result sr
-  CROSS JOIN (select count(*) as total_rules from @results_database_schema.cohort_inclusion where cohort_definition_id = @target_cohort_id) RuleTotal
-  where cohort_definition_id = @target_cohort_id and sr.inclusion_rule_mask = POWER(cast(2 as bigint),RuleTotal.total_rules)-1
-),0) as final_count
-;
+
+TRUNCATE TABLE cohort_ends;
+DROP TABLE cohort_ends;
 
 TRUNCATE TABLE inclusionRuleCohorts;
 DROP TABLE inclusionRuleCohorts;
 
-TRUNCATE TABLE PrimaryCriteriaEvents;
-DROP TABLE PrimaryCriteriaEvents;
+TRUNCATE TABLE qualified_events;
+DROP TABLE qualified_events;
+
+TRUNCATE TABLE included_events;
+DROP TABLE included_events;
+
+TRUNCATE TABLE primary_events;
+DROP TABLE primary_events;
 
 TRUNCATE TABLE Codesets;
 DROP TABLE Codesets;
