49f17148c1c5ef77ad1a4d77e2c4c06186bdd209
Maxim Moinat <maxim@thehyve.nl>
2016-09-27 14:18:45 +0200

    Added more info to drug mapping export

--- R/debugFunctions.R
+++ R/debugFunctions.R
@@ -2,7 +2,8 @@
 
 #' Export the drug mapping
 exportDrugMapping <- function(connectionDetails, export_to) {
-  query <- "SELECT * FROM etl_mappings.vnr_mapping;"
+  query <- "SELECT vnr_mapping.*, unique_varunr.atc, unique_varunr.styrknum, unique_varunr.styrka_enh, unique_varunr.styrka_tf
+            FROM etl_mappings.vnr_mapping JOIN drugmap.unique_varunr on source_concept_id = varunr;"
   connection <- connect(connectionDetails)
   result_df <- querySql(connection, query)
 
